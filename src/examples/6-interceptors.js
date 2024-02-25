cc985afc38c964b54e2277766b3cfa23c9c08774
Coderner <shreyashukla680@gmail.com>
2024-02-25 15:37:44 +0530

    interceptors

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/interceptors';
 
 const url = 'https://course-api.com/react-store-products';
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+      try{
+          const res = await authFetch("/react-store-productss");
+          // console.log(res);
+      }
+      catch(error){
+          console.log(error.response);
+      }
   };
 
   useEffect(() => {
