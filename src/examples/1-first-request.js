30ad7723bea65a143ff3fa8359910f998163bacc
Coderner <shreyashukla680@gmail.com>
2024-02-25 13:36:39 +0530

    get request

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,47 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+
+  const [products, setProducts]=useState([]);
+
+  const fetchData = async () =>{
+       try{
+            const res = await axios(url);
+            console.log(res.data);
+            setProducts(res.data);
+       }
+       catch(error){
+            console.log(error);
+       }
+  }
+
   useEffect(() => {
-    console.log('first axios request');
+      fetchData();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return (
+    <>
+       <h2 className='text-center'>first request</h2>
+       <div className='main'>
+       {
+         products && (
+           products.map((product)=>{
+             return(
+              <div key={product.id} className='card'>
+                <img src={product.image} alt={product.name} className='product-image'/>
+                <h5>{product.name}</h5>
+                <h6>{product.company}</h6>
+                <h6>{product.price}</h6>
+              </div>
+             )
+           })
+         )
+       }
+       </div>
+    </>
+  );
 };
 export default FirstRequest;
