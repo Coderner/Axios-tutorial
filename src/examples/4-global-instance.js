733d3489cbfc73e3cc824d2c63ec4bcc68b04085
Coderner <shreyashukla680@gmail.com>
2024-02-25 14:42:12 +0530

    global instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -1,11 +1,23 @@
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const productsUrl = 'https://course-api.com/react-store-products';
 const randomUserUrl = 'https://randomuser.me/api';
+const url = 'https://icanhazdadjoke.com/';
 
 const GlobalInstance = () => {
   const fetchData = async () => {
-    console.log('global axios instance');
+    try{
+       const res = await axios(url);
+       const res1 = await axios(productsUrl);
+       const res2 = await axios(randomUserUrl);
+       console.log(res);
+       console.log(res1);
+       console.log(res2);
+    }
+    catch(error){
+      console.log(error.response);
+    }
   };
 
   useEffect(() => {
